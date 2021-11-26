bee7499da84c683a81855a0928965142f6dd3beb
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-26 11:42:48 -0300

    Primeiro commit

---
+++ src/main/java/com/br/zup/vicente_imoveis/VicenteImoveisApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.vicente_imoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VicenteImoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VicenteImoveisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/zup/vicente_imoveis/VicenteImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.zup.vicente_imoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VicenteImoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
