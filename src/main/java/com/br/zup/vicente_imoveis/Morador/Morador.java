094fca0797bb77b6f1304d69f65ca9c6a836c804
Raquel Leão <vc.raquel@gmail.com>
2021-11-26 19:56:43 -0300

    adicionar anotacoes pertinentes a entidade Morador

--- src/main/java/com/br/zup/vicente_imoveis/Morador/Morador.java
+++ src/main/java/com/br/zup/vicente_imoveis/Morador/Morador.java
@@ -4,14 +4,20 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
+@Entity
+@Table(name = "moradores")
 @RequiredArgsConstructor
 @Getter
 @Setter
 
 public class Morador {
 
+    @Id
     private String cpf;
     private String nome;
     private String telefone;
