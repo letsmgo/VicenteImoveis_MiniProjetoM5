d2e468f26ff42d14c606c62a8b20db01dd794415
Raquel Leão <vc.raquel@gmail.com>
2021-11-26 20:07:00 -0300

    mapear na MoradorController metodo post para cadastrar morador

--- src/main/java/com/br/zup/vicente_imoveis/Morador/MoradorController.java
+++ src/main/java/com/br/zup/vicente_imoveis/Morador/MoradorController.java
@@ -1,5 +1,24 @@
 package com.br.zup.vicente_imoveis.Morador;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/moradores")
 public class MoradorController {
 
+    @Autowired
+    MoradorService moradorService;
+    @Autowired
+    ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MoradorDTO cadastrarMorador(@RequestBody MoradorDTO moradorDTO){
+        Morador morador = modelMapper.map(moradorDTO, Morador.class);
+        moradorService.salvarMorador(morador);
+        return moradorDTO;
+    }
 }
