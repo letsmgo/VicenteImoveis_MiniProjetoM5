69e8526b781ef27f174fbf2248fa33106bb1a2ec
Raquel Leão <vc.raquel@gmail.com>
2021-11-26 20:04:59 -0300

    criar metodo salvar morador na Service

--- src/main/java/com/br/zup/vicente_imoveis/Morador/MoradorService.java
+++ src/main/java/com/br/zup/vicente_imoveis/Morador/MoradorService.java
@@ -1,5 +1,14 @@
 package com.br.zup.vicente_imoveis.Morador;
 
-public class MoradorService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class MoradorService {
+    @Autowired
+    MoradorRepository moradorRepository;
+
+    public Morador salvarMorador(Morador morador){
+        return moradorRepository.save(morador);
+    }
 }
