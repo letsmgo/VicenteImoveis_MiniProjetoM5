26cc44562f258d36fde24baa5413bb86dbfef849
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-26 15:18:50 -0300

    Criar classe model Morador

---
+++ src/main/java/com/br/zup/vicente_imoveis/Morador/Morador.java
@@ -0,0 +1,12 @@
+package com.br.zup.vicente_imoveis.Morador;
+
+import java.util.Date;
+
+public class Morador {
+
+    private String cpf;
+    private String nome;
+    private String telefone;
+    private Date dataDeEntradaNoImovel;
+
+}
