60b084b9118562a2c70eff54856aa85e40369608
Raquel Leão <vc.raquel@gmail.com>
2021-11-27 14:29:48 -0300

    organizar model Morador

--- src/main/java/com/br/zup/vicente_imoveis/Morador/Morador.java
+++ src/main/java/com/br/zup/vicente_imoveis/Morador/Morador.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
