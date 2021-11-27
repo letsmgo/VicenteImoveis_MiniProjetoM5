f8ded2b8cea3f75c66aa0f66aba6d4ef58d070e4
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-26 23:12:01 -0300

    Implementar método para salvar imovel incluindo o endereço na ImoverService

--- src/main/java/com/br/zup/vicente_imoveis/Imovel/ImovelService.java
+++ src/main/java/com/br/zup/vicente_imoveis/Imovel/ImovelService.java
@@ -1,5 +1,22 @@
 package com.br.zup.vicente_imoveis.Imovel;
 
+import com.br.zup.vicente_imoveis.Endereco.EnderecoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ImovelService {
+
+    @Autowired
+    ImovelRepository imovelRepository;
+    @Autowired
+    EnderecoRepository enderecoRepository;
+
+    public Imovel salvarImovel(Imovel imovel){
+        enderecoRepository.save(imovel.getEndereco());
+        return imovelRepository.save(imovel);
+    }
+
+
 
 }
