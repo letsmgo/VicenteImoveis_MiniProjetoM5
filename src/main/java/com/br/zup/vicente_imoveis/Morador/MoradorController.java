91ec51c02ba8eef3da5f7a27de7e5995c43fb420
Raquel Leão <vc.raquel@gmail.com>
2021-11-27 14:06:01 -0300

    mapear na MoradorController metodo para exibir moradores

--- src/main/java/com/br/zup/vicente_imoveis/Morador/MoradorController.java
+++ src/main/java/com/br/zup/vicente_imoveis/Morador/MoradorController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/moradores")
 public class MoradorController {
@@ -16,9 +19,23 @@ public class MoradorController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MoradorDTO cadastrarMorador(@RequestBody MoradorDTO moradorDTO){
+    public MoradorDTO cadastrarMorador(@RequestBody MoradorDTO moradorDTO) {
         Morador morador = modelMapper.map(moradorDTO, Morador.class);
         moradorService.salvarMorador(morador);
         return moradorDTO;
     }
+
+    @GetMapping
+    public List<MoradorDTO> exibirMoradores() {
+
+        List<MoradorDTO> moradoresDTO = new ArrayList<>();
+
+        for (Morador morador : moradorService.exibirMoradoresCadastrados()) {
+            MoradorDTO moradorDTO = new MoradorDTO(morador.getCpf(), morador.getNome(),
+                    morador.getTelefone(), morador.getDataDeEntradaNoImovel());
+            moradoresDTO.add(moradorDTO);
+        }
+        return moradoresDTO;
+    }
+
 }
