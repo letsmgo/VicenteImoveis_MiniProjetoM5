1348f1aba8dffce9cca67fb9e6fb058839fbfe51
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-26 23:16:15 -0300

    Implementar método salvarImovel na ImovelController

--- src/main/java/com/br/zup/vicente_imoveis/Imovel/ImovelController.java
+++ src/main/java/com/br/zup/vicente_imoveis/Imovel/ImovelController.java
@@ -1,5 +1,25 @@
 package com.br.zup.vicente_imoveis.Imovel;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/imoveis")
 public class ImovelController {
+
+    @Autowired
+    ImovelService imovelService;
+    @Autowired
+    ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ImovelEntradaDTO cadastrarImovel(@RequestBody ImovelEntradaDTO imovelEntradaDTO){
+        Imovel imovel = modelMapper.map(imovelEntradaDTO, Imovel.class);
+        imovelService.salvarImovel(imovel);
+        return imovelEntradaDTO;
+    }
 
 }
