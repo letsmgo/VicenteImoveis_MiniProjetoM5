8a9c183b5243dc47e6093f8bfe34e3655f4d1858
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-11-29 19:40:03 -0300

    Criar Classe MoradorSaidaDTO

--- src/main/java/com/br/zup/vicente_imoveis/Morador/MoradorSaidaDTO.java
+++ src/main/java/com/br/zup/vicente_imoveis/Morador/MoradorSaidaDTO.java
@@ -1,2 +1,21 @@
-package com.br.zup.vicente_imoveis.Morador;public class MoradorSaidaDTO {
+package com.br.zup.vicente_imoveis.Morador;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Id;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class MoradorSaidaDTO {
+
+    @Id
+    private String cpf;
+    private String nome;
+    private String telefone;
+    private Date dataDeEntradaNoImovel;
+
 }
