1870f4a7d00cdcd258936c21d77cba49fc4bd773
Raquel Leão <vc.raquel@gmail.com>
2021-11-30 16:45:32 -0300

    mapear metodo post na ContratoController para salvar contrato

--- src/main/java/com/br/zup/vicente_imoveis/Contrato/ContratoController.java
+++ src/main/java/com/br/zup/vicente_imoveis/Contrato/ContratoController.java
@@ -1,11 +1,13 @@
 package com.br.zup.vicente_imoveis.Contrato;
 
+import com.br.zup.vicente_imoveis.Contrato.dto.ContratoEntradaDTO;
+import com.br.zup.vicente_imoveis.Contrato.dto.ContratoSaidaDTO;
 import com.br.zup.vicente_imoveis.Imovel.ImovelService;
 import com.br.zup.vicente_imoveis.cliente.ClienteService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contratos")
@@ -14,5 +16,13 @@ public class ContratoController {
     ContratoService contratoService;
     @Autowired
     ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContratoSaidaDTO salvarContrato(@RequestBody ContratoEntradaDTO contratoEntradaDTO){
+        Contrato contrato = contratoService.salvarContrato(contratoEntradaDTO.getId_imovel(),
+                contratoEntradaDTO.getId_morador());
+        return modelMapper.map(contrato, ContratoSaidaDTO.class);
+    }
 
 }
--- src/main/java/com/br/zup/vicente_imoveis/Contrato/ContratoRepository.java
+++ src/main/java/com/br/zup/vicente_imoveis/Contrato/ContratoRepository.java
@@ -3,4 +3,5 @@ package com.br.zup.vicente_imoveis.Contrato;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ContratoRepository extends CrudRepository<Contrato, Integer> {
+
 }
