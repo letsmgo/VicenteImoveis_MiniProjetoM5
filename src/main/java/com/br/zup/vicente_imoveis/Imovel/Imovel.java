c26fdb2506a969b63e3b6a51406ce42bac08b47f
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-30 10:31:07 -0300

    Remover relação da lista Moradores da model Imovel

--- src/main/java/com/br/zup/vicente_imoveis/Imovel/Imovel.java
+++ src/main/java/com/br/zup/vicente_imoveis/Imovel/Imovel.java
@@ -2,13 +2,12 @@ package com.br.zup.vicente_imoveis.Imovel;
 
 import com.br.zup.vicente_imoveis.Endereco.Endereco;
 import com.br.zup.vicente_imoveis.Imovel.Enums.TipoDeContrato;
-import com.br.zup.vicente_imoveis.Morador.Morador;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name = "imoveis")
@@ -22,13 +21,10 @@ public class Imovel {
     private int id;
     private double valor;
     private String tipoDeImovel;
-    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    private List<Morador> moradores;
     private int qtdBanheiros;
     @OneToOne
     private Endereco endereco;
     private double metragem;
     private TipoDeContrato tipoDeContrato;
-    private boolean ocupado;
 
 }
