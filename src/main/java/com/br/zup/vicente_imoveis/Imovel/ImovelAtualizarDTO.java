f643478824e58c9525cf56c12c31035ac08b4188
Let Marçal <leticiamarcalmatias@gmail.com>
2021-11-30 13:46:21 -0300

    Criar DTO para receber dados de atualização do imóvel

---
+++ src/main/java/com/br/zup/vicente_imoveis/Imovel/ImovelAtualizarDTO.java
@@ -0,0 +1,19 @@
+package com.br.zup.vicente_imoveis.Imovel;
+
+import com.br.zup.vicente_imoveis.Imovel.Enums.TipoDeContrato;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class ImovelAtualizarDTO {
+
+    private double valor;
+    private String tipoDeImovel;
+    private int qtdBanheiros;
+    private double metragem;
+    private TipoDeContrato tipoDeContrato;
+
+}
