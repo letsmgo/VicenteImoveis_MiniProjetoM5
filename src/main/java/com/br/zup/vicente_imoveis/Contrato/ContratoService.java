19521e16aa5b53f3f5981e4d6714c5d6a96720a6
Raquel Leão <vc.raquel@gmail.com>
2021-11-30 16:36:30 -0300

    criar classe ContratoService

--- src/main/java/com/br/zup/vicente_imoveis/Contrato/ContratoService.java
+++ src/main/java/com/br/zup/vicente_imoveis/Contrato/ContratoService.java
@@ -1,2 +1,17 @@
-package com.br.zup.vicente_imoveis.Contrato;public class ContratoService {
+package com.br.zup.vicente_imoveis.Contrato;
+
+import com.br.zup.vicente_imoveis.Imovel.ImovelService;
+import com.br.zup.vicente_imoveis.cliente.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContratoService {
+    @Autowired
+    ContratoRepository contratoRepository;
+    @Autowired
+    ImovelService imovelService;
+    @Autowired
+    ClienteService clienteService;
+
 }
