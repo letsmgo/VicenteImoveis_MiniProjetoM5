bc3c20e1007c2391125e413ee91b7188740b2bfc
Raquel Leão <vc.raquel@gmail.com>
2021-11-30 16:26:56 -0300

    criar classe ContratoSaidaDTO

--- src/main/java/com/br/zup/vicente_imoveis/Contrato/dto/ContratoSaidaDTO.java
+++ src/main/java/com/br/zup/vicente_imoveis/Contrato/dto/ContratoSaidaDTO.java
@@ -1,2 +1,22 @@
-package com.br.zup.vicente_imoveis.Contrato.dto;public class ContratoSaidaDTO {
+package com.br.zup.vicente_imoveis.Contrato.dto;
+
+import com.br.zup.vicente_imoveis.Contrato.enun.StatusDoContrato;
+import com.br.zup.vicente_imoveis.Imovel.Imovel;
+import com.br.zup.vicente_imoveis.cliente.Cliente;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class ContratoSaidaDTO {
+    private int id;
+    private Imovel imovel;
+    private Cliente cliente;
+    private StatusDoContrato statusDoContrato;
+    private LocalDate dataDoContrato;
+
 }
