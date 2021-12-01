a62d77848a315f79893f7f2b85d4e96e8926a97f
Raquel Leão <vc.raquel@gmail.com>
2021-11-30 21:35:53 -0300

    verificar se endereço esta cadastrado no banco de dados

--- src/main/java/com/br/zup/vicente_imoveis/Endereco/EnderecoService.java
+++ src/main/java/com/br/zup/vicente_imoveis/Endereco/EnderecoService.java
@@ -1,13 +1,36 @@
 package com.br.zup.vicente_imoveis.Endereco;
 
+import com.br.zup.vicente_imoveis.Imovel.Imovel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class EnderecoService {
     @Autowired
     EnderecoRepository enderecoRepository;
 
-    public Endereco salvarEndereco(Endereco endereco){
+    public Endereco salvarEndereco(Endereco endereco) {
         return enderecoRepository.save(endereco);
     }
+
+    public List<Endereco> exibirEnderecosCadastrados() {
+        Iterable<Endereco> endereco = enderecoRepository.findAll();
+        return (List<Endereco>) endereco;
+    }
+
+    public boolean enderecoExiste(Endereco enderecoEntrada) {
+        boolean enderecoexiste = false;
+        for (Endereco endereco : exibirEnderecosCadastrados()) {
+            if (endereco.getCep().equals(enderecoEntrada.getCep())
+                    && endereco.getLogradouro().equals(enderecoEntrada.getLogradouro())
+                    && endereco.getNumero() == enderecoEntrada.getNumero()) {
+                enderecoexiste = true;
+            }
+        }
+        return enderecoexiste;
+    }
+
 
 }
