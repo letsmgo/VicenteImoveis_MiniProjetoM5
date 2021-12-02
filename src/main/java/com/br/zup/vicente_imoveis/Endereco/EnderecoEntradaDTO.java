f91e2fb1904f09dbd071317d31615050384ab870
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-12-02 09:07:56 -0300

    Criar validação para endereço

--- src/main/java/com/br/zup/vicente_imoveis/Endereco/EnderecoEntradaDTO.java
+++ src/main/java/com/br/zup/vicente_imoveis/Endereco/EnderecoEntradaDTO.java
@@ -4,16 +4,26 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+
 @RequiredArgsConstructor
 @Getter
 @Setter
 public class EnderecoEntradaDTO {
 
+    @NotBlank(message = "O cep não pode ser vazio")
     private String cep;
+    @NotBlank(message = "Informe o logradouro com o nome")
     private String logradouro;
+    @NotBlank(message = "Informe o número do endereço")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Valor abaixo do permitido")
     private int numero;
     private String complemento;
+    @NotBlank(message = "Informe o nome do bairro")
     private String bairro;
+    @NotBlank(message = "Informe o nome da cidade")
     private String cidade;
+    @NotBlank(message = "Informe o Nome do Estado")
     private String estado;
 }
