9d0521adbf5d504985008bb121aac7e74fd7d9b9
Let Marçal <leticiamarcalmatias@gmail.com>
2021-12-02 18:39:28 -0300

    Criar classe RuntimeException para caso o cliente não possa ser deletado

---
+++ src/main/java/com/br/zup/vicente_imoveis/Custom_exception/VinculoContratualClienteException.java
@@ -0,0 +1,8 @@
+package com.br.zup.vicente_imoveis.Custom_exception;
+
+public class VinculoContratualClienteException extends RuntimeException{
+
+    public VinculoContratualClienteException(String message) {
+        super(message);
+    }
+}
