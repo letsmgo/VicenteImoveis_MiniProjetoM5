42c519818f15eb365cb967abd142ad6404620561
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-12-01 21:01:33 -0300

    Criar classe ControllerAdvice com método para mensagem de erro

--- src/main/java/com/br/zup/vicente_imoveis/config/ControllerAdvice.java
+++ src/main/java/com/br/zup/vicente_imoveis/config/ControllerAdvice.java
@@ -1,2 +1,28 @@
-package com.br.zup.vicente_imoveis.config;public class ControllerAdvice {
+package com.br.zup.vicente_imoveis.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<ErroDeValidacao> tratarErrosDeValidacao (MethodArgumentNotValidException excecao){
+        List<ErroDeValidacao> erros = new ArrayList<>();
+
+        for (FieldError referencia : excecao.getFieldErrors()){
+            ErroDeValidacao mensagem = new ErroDeValidacao(referencia.getField(),
+                    referencia.getDefaultMessage());
+            erros.add(mensagem);
+        }
+
+        return erros;
+    }
 }
