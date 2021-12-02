320132edc781254bf6aaa2aa822816480065a9f8
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-12-02 12:04:40 -0300

    criar classe custon para imovel não cadastrado

--- src/main/java/com/br/zup/vicente_imoveis/Custom_exception/ImovelNaoCadastradoException.java
+++ src/main/java/com/br/zup/vicente_imoveis/Custom_exception/ImovelNaoCadastradoException.java
@@ -1,2 +1,9 @@
-package com.br.zup.vicente_imoveis.Custom_exception;public class ImovelNaoCadastradoException {
+package com.br.zup.vicente_imoveis.Custom_exception;
+
+public class ImovelNaoCadastradoException extends RuntimeException{
+
+    public ImovelNaoCadastradoException(String message) {
+        super(message);
+    }
+
 }
