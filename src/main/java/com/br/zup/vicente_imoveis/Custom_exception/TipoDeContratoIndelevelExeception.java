a5aa7b0e8b4422619c6c10ac055a7a096c6a2ca1
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2021-12-02 11:57:08 -0300

    Criar classe custom para contrato indelevel

--- src/main/java/com/br/zup/vicente_imoveis/Custom_exception/TipoDeContratoIndelevelExeception.java
+++ src/main/java/com/br/zup/vicente_imoveis/Custom_exception/TipoDeContratoIndelevelExeception.java
@@ -1,2 +1,9 @@
-package com.br.zup.vicente_imoveis.Custom_exception;public class TipoDeContratoIndelevelExeception {
+package com.br.zup.vicente_imoveis.Custom_exception;
+
+public class TipoDeContratoIndelevelExeception extends RuntimeException{
+
+    public TipoDeContratoIndelevelExeception(String message) {
+        super(message);
+    }
+
 }
